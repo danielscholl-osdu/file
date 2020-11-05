697842499fe0598dd31a81fdd3e655c4916ed1ce
Devesh Bajpai <DBajpai@slb.com>
2020-11-05 22:24:02 +0530

    fix pom

--- file-core/src/main/java/org/opengroup/osdu/file/constant/FileMetadataConstant.java
+++ file-core/src/main/java/org/opengroup/osdu/file/constant/FileMetadataConstant.java
@@ -2,36 +2,21 @@ package org.opengroup.osdu.file.constant;
 
 public class FileMetadataConstant {
 
-	public static final String INVALID_FILE_NAME = "Invalid filename";
-	public static final String ENTITLEMENT_SERVICE_GROUP_VIEWERS = "service.schema-service.editors";
-	public static final String ENTITLEMENT_SERVICE_GROUP_EDITORS = "service.schema-service.editors";
+	public static final String FILE_SERVICE_GROUP_VIEWERS = "service.file.viewers";
+	public static final String FILE_SERVICE_GROUP_EDITORS = "service.file.editors";
 	public static final String UTF_8 = "UTF-8";
-	public static final String DEFAULT_CONTENT_TYPE = "application/octet-stream";
 	public static final String FORWARD_SLASH = "/";
-	public static final String NEWLINE_CHAR = "\n";
 	public final static String STAGING_AREA_EXT = "staging-area";
 	public final static String PERSISTENT_AREA_EXT = "persistent-area";
-	public final static String POST = "POST";
-	public final static String GET = "GET";
-	public final static String CONTENT_TYPE = "content-type";
 
 	public final static int HTTP_CODE_400 = 400;
-	public final static int HTTP_CODE_404 = 404;
 
 
-	public static final String MSG_FORBIDDEN = "User does not have access to the requested resource";
-	public static final String UNAUTHORIZED_EXCEPTION = "User is unauthorized to perform this action";
 	public static final String INVALID_SOURCE_EXCEPTION = "Invalid source file path to copy from ";
-	public static final String BAD_INPUT = "Bad input parameter";
+
 
 	public static final String METADATA_SAVE_STARTED = "Creating metadata in store";
-	public static final String TAR_FILE_EXTENSION = ".tar.gz";
-	public static final String TARGET_PATH_REGEX = "[@,:,.]+";
-	public static final String DASH = "-";
-	public static final String DOT = ".";
 	public static final String KIND_SEPRATOR = ":";
-
-	public static final String SIGNED_URL = "SignedUrl";
 	public static final String FILE_SOURCE = "FileSource";
 
 }
