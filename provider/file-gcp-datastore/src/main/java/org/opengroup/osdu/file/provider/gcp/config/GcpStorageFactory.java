4e0e3588b016c4107de0b9d6bf5f7f9f463a5caa
Rustam Lotsmanenko (EPAM) <rustam_lotsmanenko@epam.com>
2021-08-11 11:49:43 +0000

    Remove deprecated tenant factory usage from File service (GONRG-2874)

--- provider/file-gcp-datastore/src/main/java/org/opengroup/osdu/file/provider/gcp/config/GcpStorageFactory.java
+++ provider/file-gcp-datastore/src/main/java/org/opengroup/osdu/file/provider/gcp/config/GcpStorageFactory.java
@@ -1,14 +1,15 @@
 package org.opengroup.osdu.file.provider.gcp.config;
 
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.opengroup.osdu.core.common.provider.interfaces.ITenantFactory;
 import org.opengroup.osdu.core.gcp.multitenancy.GcsMultiTenantAccess;
-import org.opengroup.osdu.core.gcp.multitenancy.TenantFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.google.cloud.storage.Storage;
 
 @Component
 public class GcpStorageFactory {
+
   @Autowired
   DpsHeaders headers;
 
@@ -16,7 +17,7 @@ public class GcpStorageFactory {
   private GcsMultiTenantAccess storageFactory;
 
   @Autowired
-  private TenantFactory tenantFactory;
+  private ITenantFactory tenantFactory;
 
   public Storage getStorage() {
     return storageFactory.get(tenantFactory.getTenantInfo(headers.getPartitionId()));
