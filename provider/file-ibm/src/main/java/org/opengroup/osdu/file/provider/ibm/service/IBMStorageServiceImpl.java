a4987e9c1d8ddc36c93f54abcf3aabdb6feb5d82
Ashwani Pandey <Ashwani.Pandey3@ibm.com>
2021-09-07 17:21:44 +0530

    fileservice api issue fixing

--- provider/file-ibm/src/main/java/org/opengroup/osdu/file/provider/ibm/service/IBMStorageServiceImpl.java
+++ provider/file-ibm/src/main/java/org/opengroup/osdu/file/provider/ibm/service/IBMStorageServiceImpl.java
@@ -290,7 +290,7 @@ public class IBMStorageServiceImpl implements IStorageService {
 			SignedUrlParameters signedParam=new SignedUrlParameters();
 			SignedUrl signedUrl=createSignedUrlFileLocation(retrivaldata.getUnsignedUrl(), headers.getAuthorization(),signedParam);
 			retrivalDataSet.put("unsignedUrl", retrivaldata.getUnsignedUrl());
-			retrivalDataSet.put("signedUrl",signedUrl);
+			retrivalDataSet.put("signedUrl",signedUrl.getUrl().toString());
 			retrivalDataSet.put("signedUrlExpiration", expiration);
 			retrivalDataSet.put("connectionString", credentials.toConnectionString());
 			retrivalDataSet.put("credentials", credentials);
