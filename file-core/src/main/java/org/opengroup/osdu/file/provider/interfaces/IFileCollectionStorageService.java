9cbf615fedd5e226779a3b313bfdec5f40a16e3c
hsaxena1230 <harshit1230@gmail.com>
2022-02-15 02:38:54 +0530

    add copy api in open spec

--- file-core/src/main/java/org/opengroup/osdu/file/provider/interfaces/IFileCollectionStorageService.java
+++ file-core/src/main/java/org/opengroup/osdu/file/provider/interfaces/IFileCollectionStorageService.java
@@ -4,39 +4,10 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.opengroup.osdu.core.common.dms.model.RetrievalInstructionsResponse;
 import org.opengroup.osdu.core.common.dms.model.StorageInstructionsResponse;
 import org.opengroup.osdu.file.model.FileRetrievalData;
-import org.opengroup.osdu.file.model.SignedUrl;
-import org.opengroup.osdu.file.model.SignedUrlParameters;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 public interface IFileCollectionStorageService {
-
-  /**
-   * Gets a signed url from an unsigned url
-   *
-   * @param unsignedUrl
-   * @param authorizationToken
-   * @param signedUrlParameters
-   * @return
-   */
-
-  default SignedUrl createSignedUrlFileLocation(String unsignedUrl, String authorizationToken, SignedUrlParameters signedUrlParameters) {
-    throw new NotImplementedException("Not implemented");
-  }
-
-  /**
-   * Creates the empty directory in storage.
-   * and get signed url for uploading the files.
-   *
-   * @param fileID file ID
-   * @param authorizationToken authorization token
-   * @param partitionID partition ID
-   * @return info about object URI, signed URL and when and who created blob.
-   */
-  default SignedUrl createSignedUrl(String fileID, String authorizationToken, String partitionID) {
-    throw new NotImplementedException("Not implemented");
-  }
 
   // stub implementation
   /**
