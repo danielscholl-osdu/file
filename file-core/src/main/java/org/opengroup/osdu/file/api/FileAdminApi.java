5d26e1bf444724ea649863fb295715bfd5becb49
Thulasi Dass <Thulasi_Dass_Subramanian@epam.com>
2023-06-26 13:14:06 +0530

    Add OpenAPI documentation

--- file-core/src/main/java/org/opengroup/osdu/file/api/FileAdminApi.java
+++ file-core/src/main/java/org/opengroup/osdu/file/api/FileAdminApi.java
@@ -1,8 +1,14 @@
 package org.opengroup.osdu.file.api;
 
-import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.opengroup.osdu.core.common.model.http.AppError;
 import org.opengroup.osdu.file.constant.FileServiceRole;
 import org.opengroup.osdu.file.service.IFileAdminService;
 import org.springframework.http.HttpStatus;
@@ -20,12 +26,23 @@ import java.util.Map;
 @RequestScope
 @Validated
 @RequiredArgsConstructor
-@Hidden
+@Tag(name = "file-admin-api", description = "File Admin API")
 public class FileAdminApi {
 
   final IFileAdminService fileAdminService;
 
 
+  @Operation(summary = "${fileAdminApi.revokeURL.summary}", description = "${fileAdminApi.revokeURL.description}",
+      security = {@SecurityRequirement(name = "Authorization")}, tags = { "file-admin-api" })
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Revoked URLs successfully."),
+      @ApiResponse(responseCode = "400", description = "Bad Request",  content = {@Content(schema = @Schema(implementation = AppError.class))}),
+      @ApiResponse(responseCode = "401", description = "Unauthorized",  content = {@Content(schema = @Schema(implementation = AppError.class))}),
+      @ApiResponse(responseCode = "403", description = "User not authorized to perform the action",  content = {@Content(schema = @Schema(implementation = AppError.class))}),
+      @ApiResponse(responseCode = "500", description = "Internal Server Error",  content = {@Content(schema = @Schema(implementation = AppError.class))}),
+      @ApiResponse(responseCode = "502", description = "Bad Gateway",  content = {@Content(schema = @Schema(implementation = AppError.class))}),
+      @ApiResponse(responseCode = "503", description = "Service Unavailable",  content = {@Content(schema = @Schema(implementation = AppError.class))})
+  })
   @PreAuthorize("@authorizationFilter.hasPermission('" + FileServiceRole.ADMIN + "')")
   @DeleteMapping("/v2/files/revokeURL")
   public ResponseEntity<Void> revokeURL(@RequestBody Map<String, String> revokeURLRequest) {
