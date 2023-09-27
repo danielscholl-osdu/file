6c017513b05a5bec927db251f7acbe5f6ac763a6
Bruce <jtyself@amazon.com>
2023-09-27 13:00:54 -0500

    Remove depreciated resources

--- provider/file-aws/src/main/java/org/opengroup/osdu/file/provider/aws/security/SecurityConfig.java
+++ provider/file-aws/src/main/java/org/opengroup/osdu/file/provider/aws/security/SecurityConfig.java
@@ -14,19 +14,21 @@
 
 package org.opengroup.osdu.file.provider.aws.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic().disable().csrf().disable();
+        return http.build();
     }
 }
