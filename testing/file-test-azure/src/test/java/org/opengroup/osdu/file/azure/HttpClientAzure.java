e66ce49051feccb46240f0f4b317793ece0fe98a
Marija Dukic <marijadukic@microsoft.com>
2025-05-15 11:18:00 -0700

    Add secretless auth for azure tests

--- testing/file-test-azure/src/test/java/org/opengroup/osdu/file/azure/HttpClientAzure.java
+++ testing/file-test-azure/src/test/java/org/opengroup/osdu/file/azure/HttpClientAzure.java
@@ -9,7 +9,12 @@ import com.google.common.base.Strings;
 public class HttpClientAzure extends HttpClient {
   @Override
   public String getAccessToken() throws IOException {
-    if (Strings.isNullOrEmpty(accessToken)) {
+    String bearerToken = System.getProperty("INTEGRATION_TESTER_ACCESS_TOKEN", System.getenv("INTEGRATION_TESTER_ACCESS_TOKEN"));
+    if(!Strings.isNullOrEmpty(bearerToken) && Strings.isNullOrEmpty(accessToken)) {
+      System.out.println("Using INTEGRATION_TESTER_ACCESS_TOKEN bearer token from environment variable");
+      accessToken = bearerToken;
+    }
+    else if (Strings.isNullOrEmpty(accessToken)) {
       String sp_id = System.getProperty("INTEGRATION_TESTER", System.getenv("INTEGRATION_TESTER"));
       String sp_secret = System.getProperty("TESTER_SERVICEPRINCIPAL_SECRET", System.getenv("TESTER_SERVICEPRINCIPAL_SECRET"));
       String tenant_id = System.getProperty("AZURE_AD_TENANT_ID", System.getenv("AZURE_AD_TENANT_ID"));
@@ -21,7 +26,12 @@ public class HttpClientAzure extends HttpClient {
 
   @Override
   public String getNoDataAccessToken() throws IOException {
-    if (Strings.isNullOrEmpty(noDataAccessToken)) {
+    String bearerToken = System.getProperty("NO_DATA_ACCESS_TESTER_ACCESS_TOKEN", System.getenv("NO_DATA_ACCESS_TESTER_ACCESS_TOKEN"));
+    if(!Strings.isNullOrEmpty(bearerToken) && Strings.isNullOrEmpty(noDataAccessToken)) {
+      System.out.println("Using NO_DATA_ACCESS_TESTER_ACCESS_TOKEN bearer token from environment variable");
+      noDataAccessToken = bearerToken;
+    }
+    else if (Strings.isNullOrEmpty(noDataAccessToken)) {
       String sp_id = System.getProperty("NO_DATA_ACCESS_TESTER", System.getenv("NO_DATA_ACCESS_TESTER"));
       String sp_secret = System.getProperty("NO_DATA_ACCESS_TESTER_SERVICEPRINCIPAL_SECRET", System.getenv("NO_DATA_ACCESS_TESTER_SERVICEPRINCIPAL_SECRET"));
       String tenant_id = System.getProperty("AZURE_AD_TENANT_ID", System.getenv("AZURE_AD_TENANT_ID"));
